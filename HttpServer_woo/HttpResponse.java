340c04df4f93184913e8f63627f165306587f762
Taegyun Woo <ted2231@gmail.com>
2013-01-29 13:20:56 +0900

    made Req&Res bone and plus StatusCode

---
+++ HttpServer_woo/HttpResponse.java
@@ -0,0 +1,79 @@
+import java.net.*;
+import java.io.*;
+import java.util.regex.*;
+
+
+public class HttpResponse {
+	private Socket socket = null;
+	//StatusLine---------------
+	private String 	versionOfHttp = null;
+	private int 	statusCode = -1;
+	private String	statusText = null;
+	//Header-------------------
+	private String 	contentType = null;
+	
+	private String 	holeHead = null;
+	//Body---------------------
+	private boolean	isBody = false;
+	private byte[]	resBody = null;
+
+	//------------------------
+	private	OutputStream out = null;
+	private	DataOutputStream outputco = null;
+
+	public HttpResponse(Socket socket) {
+		try {
+			this.socket = socket;
+			out = socket.getOutputStream();
+			outputco = new DataOutputStream(out);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		versionOfHttp = "HTTP/1.1";
+	}
+	public boolean sendResponseHead() {
+		try {
+			if(versionOfHttp == null &&  statusCode == -1 &&
+				(isBody == true && contentType ==null))
+				return false;
+
+			holeHead = versionOfHttp + " " + statusCode + statusText + "\n";
+			if(isBody) {
+				holeHead = holeHead + contentType + "\n";	
+			}
+			System.out.println(holeHead);
+
+			outputco.writeBytes(holeHead + "\n");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return true;
+	}
+
+	public void setStatus(int code) {
+		statusCode = code;
+		statusText = StatusCode.get(code);
+	}
+
+	public void setContentType(String type) {
+		isBody = true;
+		contentType = "Content-Type: " + type;
+	}
+	public void setCharset(String type) {
+		contentType = contentType + "; charset=" + type;
+	}
+	public void sendBytes(byte[] body) {
+		try {
+			outputco.writeBytes(new String(body));
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public void sendString(String body) {
+		try {
+			outputco.writeBytes(body);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
--- HttpServer_woo/HttpServer.java
+++ HttpServer_woo/HttpServer.java
@@ -4,6 +4,8 @@ import java.io.*;
 public class HttpServer {
 
 	ServerSocket serverSocket;
+	HttpRequest request;
+	HttpResponse response;
 
 	public static void main(String args[]) {
 		HttpServer server = new HttpServer();
@@ -14,52 +16,39 @@ public class HttpServer {
 		try {
 			ServerSocket serverSocket = new ServerSocket(7777);
 			Socket socket = null;
-			OutputStream out;
-			DataOutputStream outputco;
-			InputStream in;
-			DataInputStream inputco;
 
 			socket = serverSocket.accept();
 
-
-
-			out = socket.getOutputStream();
-			outputco = new DataOutputStream(out);
-			in = socket.getInputStream();
-			inputco = new DataInputStream(in);
-
-
-			byte[] check = new byte[4];
-			int checkn = 0;
-			while(true){
-				byte aa = -1;
-				aa = inputco.readByte();
-				checkn = (checkn+1)%4;
-				check[checkn] = aa;
-
-				System.out.print(new String(new byte[] {aa}));
-
-				if(check[checkn] == 10 && check[(checkn+1)%4] == 13 &&
-					check[(checkn+2)%4] == 10 && check[(checkn+3)%4] == 13)
-					break;
+			//---------Request---------//
+			request = new HttpRequest(socket);
+			System.out.print(request.getHead());
+			if(request.getIsBody()) {
+				System.out.println(request.getReqBodyToString());
 			}
-
-			String Headd = "HTTP/1.1 200 OK\nContent-Type: text/html; charset=UTF-8\n";
+			System.out.println("URI : " + request.getReqUri());
+			System.out.println("host : " + request.getHostUri());
+			System.out.println("Type : " + request.getBodyType());
+			System.out.println("Length : " + request.getBodyLength());
 			
-			outputco.writeUTF(Headd + "\n[Notice] Test Message1 from Server.\n");
+			//--------Response---------//
+			response = new HttpResponse(socket);
+			response.setStatus(StatusCode.HTTP_OK);
+			response.setContentType("text/html");
+			response.setCharset("UTF-8");
+			response.sendResponseHead();
+			response.sendString("[Notice] Test Message1 from Server.<br>\n");
+			
 			System.out.println("hahaha");
 			int count = 0;
 			int real = 0;
 			while(real < 10){
 				if(count % 1000000000 == 0) {
-					outputco.writeUTF("hahaha\n");
+					response.sendString("hahaha<br>\n");
 					real ++;
 				}
 				count ++;
 			}
 
-			inputco.close();
-			outputco.close();
 			socket.close();
 
 		} catch(IOException e) {
